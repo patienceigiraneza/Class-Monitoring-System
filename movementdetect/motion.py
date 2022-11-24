d9c692d7f56003ecef7bb5888dadbcd804b9b183
patienceigiraneza <patienceigiraneza2@gmail.com>
2022-11-24 11:43:39 +0200

    Add motion detect on motion

--- movementdetect/motion.py
+++ movementdetect/motion.py
@@ -8,18 +8,18 @@ not_okay = True
 while not_okay:
     try:
         ret, frame = cap.read()
-        cv2.imshow('frame', frame)
+        cv2.imshow('Test opening', frame)
         not_okay = False
     except:
         time.sleep(1)
-        print('Not okay')
+        print('Wait .....')
 
 frame_width = int( cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height =int( cap.get( cv2.CAP_PROP_FRAME_HEIGHT))
 
 fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
 
-out = cv2.VideoWriter("output.avi", fourcc, 5.0, (1280,720))
+# out = cv2.VideoWriter("output.mp4", fourcc, 5.0, (1280,720))
 
 ret, frame1 = cap.read()
 ret, frame2 = cap.read()
@@ -37,15 +37,15 @@ while cap.isOpened():
         for contour in contours:
             (x, y, w, h) = cv2.boundingRect(contour)
 
-            if cv2.contourArea(contour) < 900:
+            if cv2.contourArea(contour) < 700:
                 continue
             cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
             cv2.putText(frame1, "Status: {}".format('Movement'), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
                         1, (0, 0, 255), 3)
         #cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
 
-        image = cv2.resize(frame1, (1280,720))
-        out.write(image)
+        image = cv2.resize(frame1, (720,360))
+        # out.write(image)
         cv2.imshow("feed", frame1)
         frame1 = frame2
         ret, frame2 = cap.read()
@@ -58,4 +58,4 @@ while cap.isOpened():
 
 cv2.destroyAllWindows()
 cap.release()
-out.release()
+# out.release()
